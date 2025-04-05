b51e1c72e7bb1ab78956f1bd98df45544756628c
jason.liang <jason.liang@cognizant.com>
2025-04-05 14:52:22 -0700

    latest

--- notebook/agentchat_captainagent.py
+++ notebook/agentchat_captainagent.py
@@ -1,22 +1,46 @@
-#!/usr/bin/env python
-# coding: utf-8
+# ---
+# jupyter:
+#   jupytext:
+#     text_representation:
+#       extension: .py
+#       format_name: percent
+#       format_version: '1.3'
+#       jupytext_version: 1.17.0
+#   kernelspec:
+#     display_name: Python 3
+#     language: python
+#     name: python3
+# ---
 
+# %% [markdown]
 # # CaptainAgent
 # By: Jiale Liu, Linxin Song, Jieyu Zhang, Shaokun Zhang
-# 
+#
 # In this notebook, we introduce CaptainAgent, an agent enhanced with the capability to call AutoBuild to break down and solve complex tasks. AutoBuild can initiate a group chat between a group of experts and converse to solve the task. The experts in nested chat can be retrieved from agent library. The agents can be equipped with tools for advanced coding.
-# 
+#
 # ````{=mdx}
 # :::info Requirements
 # Install `ag2` with CaptainAgent:
 # ```bash
-# pip install ag2[captainagent]
+# pip install -U ag2[openai,captainagent]
 # ```
-# 
-# For more information, please refer to the [installation guide](/docs/installation/Installation).
+#
+# > **Note:** If you have been using `autogen` or `pyautogen`, all you need to do is upgrade it using:  
+# > ```bash
+# > pip install -U autogen[openai,captainagent]
+# > ```
+# > or  
+# > ```bash
+# > pip install -U pyautogen[openai,captainagent]
+# > ```
+# > as `pyautogen`, `autogen`, and `ag2` are aliases for the same PyPI package.  
+#
+#
+# For more information, please refer to the [installation guide](https://docs.ag2.ai/docs/user-guide/basic-concepts/installing-ag2).
 # :::
 # ````
 
+# %% [markdown]
 # ## Setup API endpoint
 # In order to setup API, you should create a OAI_CONFIG_LIST file. The config list should look like the following:
 # ```python
@@ -41,36 +65,29 @@
 #     },
 # ]
 # ```
-# 
+#
 # ````{=mdx}
 # :::tip
-# Learn more about configuring LLMs for agents [here](/docs/topics/llm_configuration).
+# Learn more about configuring LLMs for agents [here](https://docs.ag2.ai/docs/user-guide/basic-concepts/llm-configuration/llm-configuration).
 # :::
 # ````
 
-# In[1]:
-
-
+# %%
 import autogen
 
 config_path = "OAI_CONFIG_LIST"
-config_list = autogen.config_list_from_json(
-    config_path, filter_dict={"model": ["gpt-4o"]}
-)  # You can modify the filter_dict to select your model
+# You can modify the filter_dict to select your model
+llm_config = autogen.LLMConfig.from_json(path=config_path).where(model=["gpt-4o"])
 
-
+# %% [markdown]
 # ## Using CaptainAgent without libraries
-# We begin with demonstrating how to use CaptainAgent without retrieving from libaries. In this case, CaptainAgent will automatically generate a set of experts according to its identified subtask and initate the group chat. By default, the backbone of the LLM is set to `gpt-4o`. For instructions on configuring the backbone, refer to docs on [`nested_mode`](https://docs.ag2.ai/docs/topics/captainagent/configurations).
+# We begin with demonstrating how to use CaptainAgent without retrieving from libraries. In this case, CaptainAgent will automatically generate a set of experts according to its identified subtask and initiate the group chat. By default, the backbone of the LLM is set to `gpt-4o`. For instructions on configuring the backbone, refer to docs on [`nested_mode`](https://docs.ag2.ai/latest/docs/user-guide/reference-agents/captainagent).
 
-# In[2]:
-
-
+# %%
 from autogen import UserProxyAgent
 from autogen.agentchat.contrib.captainagent import CaptainAgent
 
-llm_config = {"temperature": 0, "config_list": config_list}
-
-## build agents
+# build agents
 captain_agent = CaptainAgent(
     name="captain_agent",
     llm_config=llm_config,
@@ -79,31 +96,27 @@ captain_agent = CaptainAgent(
 )
 captain_user_proxy = UserProxyAgent(name="captain_user_proxy", human_input_mode="NEVER")
 
-
-# In[3]:
-
-
+# %%
 result = captain_user_proxy.initiate_chat(
     captain_agent,
     message="Find a recent paper about large language models on arxiv and find its potential applications in software.",
     max_turns=1,
 )
 
-
+# %% [markdown]
 # ## Building Agents from library & Retrieve tools from tool library
-# One significant feature of CaptainAgent is that the agents and tools can be retrieved from a dedicated library. When CaptainAgent starts building experts for group chat, it will retrieve and select from agent library, then assign tools retreived from tool library to the experts.
-# 
-# For agent library, refer to [`captainagent_expert_library.json`](https://github.com/ag2ai/ag2/blob/main/notebook/captainagent_expert_library.json) for samples. You can refer to [docs](https://docs.ag2.ai/docs/topics/captainagent/agent_library) on how to customize your own expert library.
-# 
+# One significant feature of CaptainAgent is that the agents and tools can be retrieved from a dedicated library. When CaptainAgent starts building experts for group chat, it will retrieve and select from agent library, then assign tools retrieved from tool library to the experts.
+#
+# For agent library, refer to [`captainagent_expert_library.json`](https://github.com/ag2ai/ag2/blob/main/notebook/captainagent_expert_library.json) for samples. You can refer to [docs](https://docs.ag2.ai/latest/docs/user-guide/reference-agents/captainagent) on how to customize your own expert library.
+#
 # For tool library, we provide a set of tools [here](https://github.com/ag2ai/ag2/tree/main/autogen/agentchat/contrib/captainagent/tools/README.md), the tools are categorized into three types: data analysis, information_retrieval, math. If you are using the tools, you should [install the requirements](https://github.com/ag2ai/ag2/tree/main/autogen/agentchat/contrib/captainagent/tools/README.md#how-to-use) for them.
 
+# %% [markdown]
 # ### Using Agent Library Only
 # Below is an example that retrieves experts from library and build nested chat accordingly.
-# 
+#
 
-# In[5]:
-
-
+# %%
 from autogen import UserProxyAgent
 from autogen.agentchat.contrib.captainagent import CaptainAgent
 
@@ -121,27 +134,25 @@ query = "find papers on LLM applications from arxiv in the last week, create a m
 
 result = captain_user_proxy.initiate_chat(captain_agent, message=query)
 
-
+# %% [markdown]
 # ## Using Both Agent Library and Tool Library
 # Now let's retrieve from both agent library and tool library while building experts for nested chat.
-# 
-# To run the following demo, it is required to install the dependencies from the tool library and obtain BING search api and Rapid API key for tools in library to fully function. Please follow the instructions [here](https://docs.ag2.ai/docs/topics/captainagent/tool_library).
+#
+# To run the following demo, it is required to install the dependencies from the tool library and obtain BING search api and Rapid API key for tools in library to fully function. Please follow the instructions [here](https://docs.ag2.ai/latest/docs/user-guide/reference-agents/captainagent).
 
-# In[ ]:
-
-
+# %%
 # The function requires BING api key and Rapid API key to work. You can follow the instructions from docs to get one.
 import os
 
 from autogen import UserProxyAgent
 from autogen.agentchat.contrib.captainagent import CaptainAgent
 
-os.environ["BING_API_key"] = ""  # set your bing api key here, if you donot need search engine, you can skip this step
+os.environ["BING_API_KEY"] = ""  # set your bing api key here, if you don't need search engine, you can skip this step
 os.environ["RAPID_API_KEY"] = (
     ""  # set your rapid api key here, in order for this example to work, you need to subscribe to the youtube transcription api(https://rapidapi.com/solid-api-solid-api-default/api/youtube-transcript3)
 )
 
-## build agents
+# build agents
 captain_agent = CaptainAgent(
     name="captain_agent",
     llm_config=llm_config,
@@ -162,13 +173,14 @@ What does Teal'c say in response to the question "Isn't that hot?"
 """.strip()
 result = captain_user_proxy.initiate_chat(captain_agent, message=query)
 
-
+# %% [markdown]
 # The ground truth answer to the question is 'Extremely', which the CaptainAgent answers correctly. Notably, with the assistance of tools, the agent can answer video-related questions. This shows the huge potential of what user customized tools can bring.
 
+# %% [markdown]
 # # Further Reading
-# For a full list of the configurables and the functionalities in CaptainAgent, please refer [here](https://docs.ag2.ai/docs/topics/captainagent/configurations).
-# 
-# For how to customize your own agent library, check [here](https://docs.ag2.ai/docs/topics/captainagent/agent_library).
-# 
-# For how to implement your own tool library that suits your need, check the documents [here](https://docs.ag2.ai/docs/topics/captainagent/tool_library).
-# 
+# For a full list of the configurables and the functionalities in CaptainAgent, please refer [here](https://docs.ag2.ai/latest/docs/user-guide/reference-agents/captainagent).
+#
+# For how to customize your own agent library, check [here](https://docs.ag2.ai/latest/docs/user-guide/reference-agents/captainagent).
+#
+# For how to implement your own tool library that suits your need, check the documents [here](https://docs.ag2.ai/latest/docs/user-guide/reference-agents/captainagent).
+#
--- notebook/agentchat_captainagent_crosstool.py
+++ notebook/agentchat_captainagent_crosstool.py
@@ -1,74 +1,89 @@
-#!/usr/bin/env python
-# coding: utf-8
+# ---
+# jupyter:
+#   jupytext:
+#     text_representation:
+#       extension: .py
+#       format_name: percent
+#       format_version: '1.3'
+#       jupytext_version: 1.17.0
+#   kernelspec:
+#     display_name: Python 3
+#     language: python
+#     name: python3
+# ---
 
+# %% [markdown] vscode={"languageId": "plaintext"}
 # # Cross-Framework LLM Tool for CaptainAgent
-# In this tutorial, we demonstrate how to integrate LLM tools from [LangChain Tools](https://python.langchain.com/v0.1/docs/modules/tools), [CrewAI Tools](https://github.com/crewAIInc/crewAI-tools/tree/main) into CaptainAgent. The developers just need to use one line of code to convert them into [AG2 tools](https://docs.ag2.ai/notebooks/tools_interoperability), and then pass it to CaptainAgent while instantiation, simple as that.
+# In this tutorial, we demonstrate how to integrate LLM tools from [LangChain Tools](https://python.langchain.com/v0.1/docs/modules/tools), [CrewAI Tools](https://github.com/crewAIInc/crewAI-tools/tree/main) into CaptainAgent. The developers just need to use one line of code to convert them into [AG2 tools](https://docs.ag2.ai/docs/use-cases/notebooks/notebooks/tools_interoperability), and then pass it to CaptainAgent while instantiation, simple as that.
 
+# %% [markdown]
 # ## Langchain Tool Integration
 # Langchain readily provides a number of tools at hand. These tools can be integrated into AG2 framework through interoperability.
-# 
+#
 # ### Installation
 # To integrate LangChain tools into the AG2 framework, install the required dependencies:
-# 
+#
 # ```bash
-# pip install ag2[interop-langchain]
+# pip install -U ag2[openai,interop-langchain]
 # ```
-# 
+#
+# > **Note:** If you have been using `autogen` or `pyautogen`, all you need to do is upgrade it using:  
+# > ```bash
+# > pip install -U autogen[openai,interop-langchain]
+# > ```
+# > or  
+# > ```bash
+# > pip install -U pyautogen[openai,interop-langchain]
+# > ```
+# > as `pyautogen`, `autogen`, and `ag2` are aliases for the same PyPI package.  
+#
+#
 # Additionally, this notebook uses LangChain's [DuckDuckGo Search Tool](https://python.langchain.com/docs/integrations/tools/ddg/), which requires the `duckduckgo-search` package. Install it with:
-# 
+#
 # ```bash
 # pip install duckduckgo-search
 # ```
-# 
+#
 # ### Preparation
 # Import necessary modules and tools.
 # - [DuckDuckGoSearchRun](https://python.langchain.com/api_reference/community/tools/langchain_community.tools.ddg_search.tool.DuckDuckGoSearchRun.html) and [DuckDuckGoSearchAPIWrapper](https://python.langchain.com/api_reference/community/utilities/langchain_community.utilities.duckduckgo_search.DuckDuckGoSearchAPIWrapper.html#langchain_community.utilities.duckduckgo_search.DuckDuckGoSearchAPIWrapper): Tools for querying DuckDuckGo.
 # - `Interoperability`: This module acts as a bridge, making it easier to integrate LangChain tools with AG2’s architecture.
 
-# In[1]:
-
-
+# %%
 from langchain_community.tools import DuckDuckGoSearchRun
 from langchain_community.utilities import DuckDuckGoSearchAPIWrapper
 
 from autogen.interop import Interoperability
 
-
+# %% [markdown]
 # ### Configure the agents
 # Load the config for LLM, which include API key and model.
 
-# In[2]:
-
-
+# %%
 import autogen
 
 config_path = "OAI_CONFIG_LIST"
-config_list = autogen.config_list_from_json(config_path, filter_dict={"model": ["gpt-4o"]})
-llm_config = {"temperature": 0, "config_list": config_list}
+llm_config = autogen.LLMConfig.from_json(path=config_path, temperature=0).where(model=["gpt-4o"])
 
-
+# %% [markdown]
 # ### Tool Integration
 # We use `Interoperability` to convert the LangChain tool into a format compatible with the AG2 framework.
 
-# In[3]:
-
-
+# %%
 interop = Interoperability()
 
 api_wrapper = DuckDuckGoSearchAPIWrapper()
 langchain_tool = DuckDuckGoSearchRun(api_wrapper=api_wrapper)
 ag2_tool = interop.convert_tool(tool=langchain_tool, type="langchain")
 
-
+# %% [markdown]
 # Then add the tools to CaptainAgent, the main difference from original CaptainAgent initialization is to pass the tool as a list into the `tool_lib` argument. This will let the agents within the nested chat created by CaptainAgent all equipped with the tools. THey can write python code to call the tools and observe the results.
 
-# In[4]:
-
-
+# %%
 from autogen import UserProxyAgent
 from autogen.agentchat.contrib.captainagent import CaptainAgent
 
-## build agents
+# build agents
 captain_agent = CaptainAgent(
     name="captain_agent",
     llm_config=llm_config,
@@ -79,33 +94,38 @@ captain_agent = CaptainAgent(
 )
 captain_user_proxy = UserProxyAgent(name="captain_user_proxy", human_input_mode="NEVER")
 
-
-# In[5]:
-
-
+# %%
 res = captain_user_proxy.initiate_chat(
     captain_agent,
     message="Call a group of experts and search for the word of the day Merriham Webster December 26, 2024",
 )
 
-
+# %% [markdown]
 # ## CrewAI Tool Integration
 # CrewAI also provides a variety of powerful tools designed for tasks such as web scraping, search, code interpretation, and more. The full list of available tools in CrewAI can be observed [here](https://github.com/crewAIInc/crewAI-tools/tree/main).
-# 
+#
 # ### Installation
 # Install the required packages for integrating CrewAI tools into the AG2 framework.
 # This ensures all dependencies for both frameworks are installed.
-# 
+#
 # ```bash
-# pip install ag2[interop-crewai]
+# pip install -U ag2[openai,interop-crewai]
 # ```
-# 
+#
+# > **Note:** If you have been using `autogen` or `pyautogen`, all you need to do is upgrade it using:  
+# > ```bash
+# > pip install -U autogen[openai,interop-crewai]
+# > ```
+# > or  
+# > ```bash
+# > pip install -U pyautogen[openai,interop-crewai]
+# > ```
+# > as `pyautogen`, `autogen`, and `ag2` are aliases for the same PyPI package.  
+#
 # ### Tool Integration
 # Integrating CrewAI tools into AG2 framework follows a similar pipeline as shown below.
 
-# In[6]:
-
-
+# %%
 from crewai_tools import ScrapeWebsiteTool
 
 from autogen.interop import Interoperability
@@ -114,17 +134,15 @@ interop = Interoperability()
 crewai_tool = ScrapeWebsiteTool()
 ag2_tool = interop.convert_tool(tool=crewai_tool, type="crewai")
 
-
+# %% [markdown]
 # ### Adding tools to CaptainAgent
 # The process is identical to the above, pass the converted tool to `tool_lib` argument, and all the agents created by CaptainAgent gets access to the tools.
 
-# In[7]:
-
-
+# %%
 from autogen import UserProxyAgent
 from autogen.agentchat.contrib.captainagent import CaptainAgent
 
-## build agents
+# build agents
 captain_agent = CaptainAgent(
     name="captain_agent",
     llm_config=llm_config,
@@ -135,16 +153,9 @@ captain_agent = CaptainAgent(
 )
 captain_user_proxy = UserProxyAgent(name="captain_user_proxy", human_input_mode="NEVER")
 
-
-# In[8]:
-
-
+# %%
 message = "Call experts and Scrape the website https://ag2.ai/, analyze the content and summarize it"
 result = captain_user_proxy.initiate_chat(captain_agent, message=message)
 
-
-# In[9]:
-
-
+# %%
 print(result.summary)
-
